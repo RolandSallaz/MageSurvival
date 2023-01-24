32af0c41fd5cfb7beeba45f3044f622d1414bcda
unknown <machodin@yandex.ru>
2023-01-25 02:00:46 +0300

    fix movement and directin bug

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const enemy = new Enemy()
 
 setInterval(() => {
   hp.textContent = `HP: ${player.hp}`
-  enemy.move()
   player.updateMove()
   player.gameObject.scrollIntoView({ block: 'center', inline: 'center' })
 }, 1000 / 60)
@@ -17,10 +16,10 @@ setInterval(() => {
 }, 1000 / player.attackPs)
 
 document.addEventListener('mousedown', (e) =>
-  player.move({ cords: { x: e.offsetX, y: e.offsetY } }),
+  player.targetPosition={ x: e.offsetX, y: e.offsetY }
 )
 document.addEventListener('mousemove', (e) => {
-  if (e.clientX > Number(player.position.x)) {
+  if (e.offsetX > Number(player.position.x)) {
     player.directon = 1
   } else {
     player.directon = -1
--- modules/Player.js
+++ modules/Player.js
@@ -1,7 +1,10 @@
 class Player {
   constructor() {
-    this.position = { x: window.screen.width / 2, y: window.screen.height / 2 }
-    this.targetPosition = this.position
+    this.position = {
+      x: document.body.clientWidth / 2,
+      y: document.body.clientHeight / 2,
+    }
+    this.targetPosition = null
     this.gameObject = document.querySelector('.player')
     this.speed = 2
     this.attackPs = 1
@@ -9,27 +12,28 @@ class Player {
     this.directon = 1
   }
 
-  async move({ cords }) {
-    this.targetPosition = cords
-  }
   attack(charge) {
     charge.spawn(this.directon)
   }
   updateMove() {
     this.gameObject.style.transform = `scale(${this.directon},1)`
-    if (this.position.x == this.targetPosition.x - this.speed) return
-    if (this.position.x == this.targetPosition.x + this.speed) return
-    if (this.position.y == this.targetPosition.y - this.speed) return
-    if (this.position.y == this.targetPosition.y + this.speed) return
-    if (this.position.x >= this.targetPosition.x) {
-      this.position.x -= this.speed
-    } else {
-      this.position.x += this.speed
-    }
-    if (this.position.y >= this.targetPosition.y) {
-      this.position.y -= this.speed
-    } else {
-      this.position.y += this.speed
+
+    if (this.targetPosition) {
+      if (this.position.x == this.targetPosition.x - this.speed) return
+      if (this.position.x == this.targetPosition.x + this.speed) return
+      if (this.position.y == this.targetPosition.y - this.speed) return
+      if (this.position.y == this.targetPosition.y + this.speed) return
+
+      if (this.position.x >= this.targetPosition.x) {
+        this.position.x -= this.speed
+      } else {
+        this.position.x += this.speed
+      }
+      if (this.position.y >= this.targetPosition.y) {
+        this.position.y -= this.speed
+      } else {
+        this.position.y += this.speed
+      }
     }
     this.gameObject.style.left = `${this.position.x}px`
     this.gameObject.style.top = `${this.position.y}px`
