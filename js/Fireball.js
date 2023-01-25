365761e9a2ff53cb53255450b5b8aaab53b57c9d
Roland <machodin@yandex.ru>
2023-01-26 02:42:21 +0300

    fix fireball start position

--- js/Fireball.js
+++ js/Fireball.js
@@ -1,32 +1,36 @@
-import { main,playerElement } from "./constants.js";
-import { player } from "./Player.js";
+import { main, playerElement } from './constants.js'
+import { player } from './Player.js'
 
-class Fireball{
-    constructor(){
-        this.charges = []
-        this.chageSpeed = 10;
+class Fireball {
+  constructor() {
+    this.charges = []
+    this.chageSpeed = 10
+  }
+  spawn(direction) {
+    const element = document.createElement('div')
+    element.classList.add('fireball')
+    if (player.directon == 1) {
+      element.style.transform = 'rotate(180deg)'
     }
-    spawn(direction){
-        const element = document.createElement('div');
-        element.classList.add('fireball')
-        if (player.directon == 1){
-            element.style.transform = 'rotate(180deg)'
-        }
-        element.position = {x:Number(playerElement.style.left.replace('px','')),y:Number(playerElement.style.top.replace('px',''))};
-        this.charges.push(element)
-        main.append(element)
-        setTimeout(()=>{
-            this.charges.shift()
-            element.remove()
-          },1000)
-        setInterval(()=>{
-            element.position.x = direction == 1 ? (Number(element.position.x += this.chageSpeed)) : (Number(element.position.x -= this.chageSpeed));
-            element.style.top = `${element.position.y}px`
-            element.style.left = `${element.position.x}px`
-        },1000/60)
-
+    element.position = {
+      x: Number(playerElement.style.left.replace('px', '')) + 30 * player.directon,
+      y: Number(playerElement.style.top.replace('px', '')) + 10,
     }
-
+    this.charges.push(element)
+    main.append(element)
+    setTimeout(() => {
+      this.charges.shift()
+      element.remove()
+    }, 1000)
+    setInterval(() => {
+      element.position.x =
+        direction == 1
+          ? Number((element.position.x += this.chageSpeed))
+          : Number((element.position.x -= this.chageSpeed))
+      element.style.top = `${element.position.y}px`
+      element.style.left = `${element.position.x}px`
+    }, 1000 / 60)
+  }
 }
 
-export const fireball = new Fireball();
+export const fireball = new Fireball()
